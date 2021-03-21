31bbbd03dd36b33e901884f6f1aaa4bd377f3cdb
Barry Porch <bporch@ymail.com>
2021-03-21 14:10:49 -0400

    Finished MVP

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
 import React from "react";
 import { Switch, Route, Link } from "react-router-dom"
 import Form from "./components/Form"
+import styled from "styled-components"
+
+const StyledHeader = styled.div`
+h1, h2, p {
+  text-align: center;
+}
+
+h1 {
+  font-size: 100px;
+  margin: 0;
+  text-shadow: 2px 2px 5px red;
+}
+
+h2 {
+  font-size: 35px;
+  color: red;
+  text-shadow: 2px 2px 5px black;
+  margin-top: 0;
+}
+
+p {
+  font-size: 20px;
+  padding: 10px 25px;
+  background-color: red;
+  color: black;
+  border: 3px solid black;
+  box-shadow: 2px 2px 5px black;
+  width: 5%;
+  margin: 0 auto;
+  margin-bottom: 50px;
+}
+`
 
 const App = () => {
   return (
-    <>
+    <StyledHeader>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-      <Link to="/pizza">Pizza?</Link>
+      <h2>NOM NOM NOM NOM</h2>
+      <Link style={{textDecoration: "none"}} to="/pizza"><p>Pizza?</p></Link>
       <Switch>
         <Route path="/pizza" component={Form} />
         <Route path ="/" />
       </Switch>
-    </>
+    </StyledHeader>
   );
 };
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,28 @@
 import React, { useState, useEffect } from "react"
 import axios from "axios"
 import * as yup from "yup"
+import styled from "styled-components"
+
+const StyledForm = styled.form`
+border: 1px black solid;
+display: flex;
+flex-direction: column;
+text-align: center;
+background-color: red;
+width: 80%;
+margin: 0 auto;
+
+div {
+    padding: 20px 0;
+}
+
+button {
+    font-size: 20px;
+    padding: 10px 25px;
+    border: 3px solid black;
+    box-shadow: 2px 2px 5px black;
+}
+`
 
 const schema = yup.object().shape({
     name: yup.string().trim().required("Name is required").min(2, "Must be at least 2 characters long."),
@@ -84,92 +106,106 @@ const Form = () => {
      }, [form])
 
     return (
-        <form onSubmit={handleSubmit}>
+        <StyledForm onSubmit={handleSubmit}>
 
-            <input
-                type="text"
-                name="name"
-                placeholder="Enter name here"
-                onChange={changeHandler}
-                value={form.name}
-            />
+            <div>
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="Enter name here"
+                    onChange={changeHandler}
+                    value={form.name}
+                />
+            </div>
 
-            <label>Pizza Size</label>
-            <select
-                name="size"
-                onChange={changeHandler}
-            >
-                <option value="">--Select a size--</option>
-                <option value="small">Small</option>
-                <option value="medium">Medium</option>
-                <option value="large">Large</option>
-                <option value="extralarge">Extra Large</option>
-            </select>
+            <div>
+                <label>Pizza Size: </label>
+                <select
+                    name="size"
+                    onChange={changeHandler}
+                >
+                    <option value="">--Select a size--</option>
+                    <option value="small">Small</option>
+                    <option value="medium">Medium</option>
+                    <option value="large">Large</option>
+                    <option value="extralarge">Extra Large</option>
+                </select>
+            </div>
 
-            <input
-                type="checkbox"
-                name="pepperoni"
-                checked={form.pepperoni}
-                onChange={changeHandler}
-            />
+            <div>
+                <input
+                    type="checkbox"
+                    name="pepperoni"
+                    checked={form.pepperoni}
+                    onChange={changeHandler}
+                />
+    
+                <label>
+                    Pepperoni
+                </label>
+            </div>
 
-            <label>
-                Pepperoni
-            </label>
+            <div>
+                <input
+                    type="checkbox"
+                    name="mushrooms"
+                    checked={form.mushrooms}
+                    onChange={changeHandler}
+                />
 
-            <input
-                type="checkbox"
-                name="mushrooms"
-                checked={form.mushrooms}
-                onChange={changeHandler}
-            />
+                <label>
+                    Mushrooms
+                </label>
+            </div>
 
-            <label>
-                Mushrooms
-            </label>
+            <div>
+                <input
+                    type="checkbox"
+                    name="onions"
+                    checked={form.onions}
+                    onChange={changeHandler}
+                />
             
-            <input
-                type="checkbox"
-                name="onions"
-                checked={form.onions}
-                onChange={changeHandler}
-            />
-            
-            <label>
-                Onions
-            </label>
-
-            <input
-                type="checkbox"
-                name="sausage"
-                checked={form.sausage}
-                onChange={changeHandler}
-            />
-
-            <label>
-                Sausage
-            </label>
-
-            <textarea
-                rows={5}
-                cols={50}
-                placeholder="Enter special instructions here"
-                type="text"
-                onChange={changeHandler}
-                value={form.instructions}
-                name="instructions"
-            >
+                <label>
+                    Onions
+                </label>
+            </div>
                 
-            </textarea>
+            <div>
+                <input
+                    type="checkbox"
+                    name="sausage"
+                    checked={form.sausage}
+                    onChange={changeHandler}
+                />
 
-            <button
-                type="submit"
-                disabled={disabled}
-            >
-                Submit Order
-            </button>
+                <label>
+                    Sausage
+                </label>
+            </div>
 
-        </form>
+            <div>
+                <textarea
+                    rows={5}
+                    cols={50}
+                    placeholder="Enter special instructions here"
+                    type="text"
+                    onChange={changeHandler}
+                    value={form.instructions}
+                    name="instructions"
+                / >
+            </div>
+
+            <div>
+                <button
+                    type="submit"
+                   disabled={disabled}
+                >
+                   Submit Order
+                </button>
+            </div>
+
+        </StyledForm>
     )
 }
 
