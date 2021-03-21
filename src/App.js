453b2be982c662ae080536987c00855c541fde98
Barry Porch <bporch@ymail.com>
2021-03-21 11:25:38 -0400

    Finished Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Switch, Route, Link } from "react-router-dom"
+import Form from "./components/Form"
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>HEADER</p>
+      <p>You can remove this code and create your own header</p>
+      <Link to="/pizza">Pizza?</Link>
+      <Switch>
+        <Route path="/pizza" component={Form} />
+        <Route path ="/" />
+      </Switch>
     </>
   );
 };
---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Form = () => {
+    return (
+        <diV>
+            <h1>This is the form!</h1>
+        </diV>
+    )
+}
+
+export default Form
